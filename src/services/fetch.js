83dffe953bcdcda73b2e506483901bd71ec09ac8
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-05-23 22:04:01 +0300

    Adds style index.css, folder services, file fetch.js, libraries: axios, prop-types, react-hot-toast, react-loader-spinner, styled-components.

---
+++ src/services/fetch.js
@@ -0,0 +1,14 @@
+const API_KEY = `33671323-2cce06c3be5a372be22004315`;
+const BASE_URL = `https://pixabay.com/api/`;
+
+export async function fetchImages(searchQuery, page) {
+  const response = await fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&page=${page}&per_page=12`
+  );
+  if (!response.ok) {
+    const message = `Failed to fetch images: ERROR ${response.status}`;
+    throw new Error(message);
+  }
+  const data = await response.json();
+  return data;
+}
